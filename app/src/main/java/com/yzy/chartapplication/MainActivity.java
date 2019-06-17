7130a80bc1ee4369fd6daf311e533f921e4e6685
chenw@eagersoft.cn <cw123456>
2019-06-17 18:47:55 +0800

    提交代码2

--- app/src/main/java/com/yzy/chartapplication/MainActivity.java
+++ app/src/main/java/com/yzy/chartapplication/MainActivity.java
@@ -3,8 +3,14 @@ package com.yzy.chartapplication;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.github.mikephil.charting.data.PieEntry;
 import com.yzy.chartapplication.chart.YouzyBarChart;
 import com.yzy.chartapplication.chart.YouzyPieChart;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,9 +23,31 @@ public class MainActivity extends AppCompatActivity {
         YouzyBarChart barChart = findViewById(R.id.chart1);
         YouzyPieChart pieChart = findViewById(R.id.chart2);
 
-//        barChart.setBarChart(null,null,"");
+        /*java.util.List<String> xAxisValue = new ArrayList<>();
+        List<Float> yAxisValue = new ArrayList<>();
 
-//        pieChart.setData(null);
+        xAxisValue.add("武汉");
+        xAxisValue.add("深圳");
+        xAxisValue.add("广州");
+        xAxisValue.add("上海");
+        xAxisValue.add("北京");
+
+        yAxisValue.add(21f);
+        yAxisValue.add(18f);
+        yAxisValue.add(16f);
+        yAxisValue.add(14f);
+        yAxisValue.add(10f);
+        barChart.setBarChart(xAxisValue,yAxisValue,"就业地区");*/
+
+        ArrayList<PieEntry> entries = new ArrayList<>();
+        entries.add(new PieEntry(0.4f, "武汉"));
+        entries.add(new PieEntry(0.28f, "深圳"));
+        entries.add(new PieEntry(0.12f, "广州"));
+        entries.add(new PieEntry(0.1f, "上海"));
+        entries.add(new PieEntry(0.05f, "北京"));
+        entries.add(new PieEntry(0.05f, "其它"));
+        pieChart.setData(entries, false, 1,true);
+
     }
 
 }
--- app/src/main/java/com/yzy/chartapplication/chart/YouzyPieChart.java
+++ app/src/main/java/com/yzy/chartapplication/chart/YouzyPieChart.java
@@ -39,13 +39,31 @@ public class YouzyPieChart extends PieChart {
     private float pieChartRotationAngle = -90f;
     //传入的数据
     private ArrayList<PieEntry> entries = new ArrayList<>();
+    //每个的比例颜色
+    private int pieDrawValuesColor = Color.rgb(74, 74, 74);
+    //设置图表点击Item高亮是否可用
+    private boolean isHighlightPerTapEnabled = true;
+    //设置图表是否显示文字可用
+    private boolean isDrawCenterText = false;
+    //使用的颜色类别
+    private int colorsType = 0;
     //饼状图颜色
     private final int[] PIE_COLORS = {
-            Color.rgb(233,48,45),
+            Color.rgb(233, 48, 45),
             Color.rgb(232, 232, 232),
             Color.rgb(232, 232, 232),
             Color.rgb(232, 232, 232),
             Color.rgb(232, 232, 232)
+    };
+
+
+    private final int[] PIE_COLORS_1 = {
+            Color.rgb(88, 114, 239),
+            Color.rgb(44, 188, 239),
+            Color.rgb(100, 221, 184),
+            Color.rgb(248, 207, 81),
+            Color.rgb(251, 228, 148),
+            Color.rgb(216, 230, 253)
     };
 
     public YouzyPieChart(Context context) {
@@ -78,6 +96,25 @@ public class YouzyPieChart extends PieChart {
     }
 
     /**
+     * 设置饼图样式
+     *
+     * @param pieValues 数据源
+     */
+    public YouzyPieChart setData(ArrayList<PieEntry> pieValues, boolean isHighlightPerTapEnabled, int colorsType,boolean isDrawCenterText) {
+        this.isHighlightPerTapEnabled = isHighlightPerTapEnabled;
+        this.isDrawCenterText = isDrawCenterText;
+        this.colorsType = colorsType;
+        //图例设置
+        setPieChartLegendEnabled(false);
+        //设置饼状图属性
+        setPieChartAttributes();
+        //设置饼图数据
+        entries.addAll(pieValues);
+        pieChartInvalidate();
+        return this;
+    }
+
+    /**
      * 给饼状图添加点击事件
      */
     public void addChartValueSelectedListener(final OnPieChartItemClick onPieChartItemClick) {
@@ -86,7 +123,7 @@ public class YouzyPieChart extends PieChart {
             @Override
             public void onValueSelected(Entry e, Highlight h) {
                 int position = (int) h.getX();
-                updatePieChartColor(position);
+//                updatePieChartColor(position);
 
                 pieChartInvalidate();
                 onPieChartItemClick.onPieChartItemClick(position);
@@ -104,9 +141,9 @@ public class YouzyPieChart extends PieChart {
      */
     private void updatePieChartColor(int position) {
         for (int i = 0; i < PIE_COLORS.length; i++) {
-            if (position == i){
-                PIE_COLORS[i] = Color.rgb(233,48,45);
-            }else {
+            if (position == i) {
+                PIE_COLORS[i] = Color.rgb(233, 48, 45);
+            } else {
                 PIE_COLORS[i] = Color.rgb(232, 232, 232);
             }
         }
@@ -139,14 +176,21 @@ public class YouzyPieChart extends PieChart {
         getDescription().setEnabled(false);//设置描述
         setExtraOffsets(leftExtraOffsets, topExtraOffsets, rightExtraOffsets, bottomExtraOffsets);
 //        setCenterText(title);//设置环中的文字
-        setCenterTextSize(pieChartCenterTextSize);//设置环中文字的大小
-        setDrawCenterText(true);//设置绘制环中文字
-        setRotationAngle(pieChartRotationAngle);//设置旋转角度
+//        setCenterTextSize(pieChartCenterTextSize);//设置环中文字的大小
+//        setDrawCenterText(true);//设置绘制环中文字
+        //是否绘制PieChart内部中心文本
+        setDrawCenterText(isDrawCenterText);
         //设置是否绘制条目标签名称
-        setDrawEntryLabels(false);
+        setDrawEntryLabels(isDrawCenterText);
+        // 设置图表点击Item高亮是否可用
+        setHighlightPerTapEnabled(isHighlightPerTapEnabled);
+        //设置旋转角度
+        setRotationAngle(pieChartRotationAngle);
         //设置是否启用空洞
         setDrawHoleEnabled(true);
         animateX(mAnimateDurationMillis, Easing.EasingOption.EaseInOutQuad);//数据显示动画
+        // 设置pieChart图表是否可以手动旋转
+        setRotationEnabled(false);
     }
 
     /**
@@ -171,19 +215,21 @@ public class YouzyPieChart extends PieChart {
         dataSet.setSliceSpace(pieChartSliceSpace);//设置饼块之间的间隔
         dataSet.setSelectionShift(5f);//设置饼块选中时偏离饼图中心的距离
 
-        dataSet.setColors(PIE_COLORS);//设置饼块的颜色
+        dataSet.setColors(colorsType == 0 ? PIE_COLORS : PIE_COLORS_1);//设置饼块的颜色
         dataSet.setValueLinePart1OffsetPercentage(80f);//数据连接线距图形片内部边界的距离，为百分数
         dataSet.setValueLinePart1Length(0.3f);
         dataSet.setValueLinePart2Length(0.4f);
-        dataSet.setValueLineColor(Color.BLUE);//设置连接线的颜色
+        dataSet.setValueLineColor(pieDrawValuesColor);//设置连接线的颜色
         dataSet.setYValuePosition(PieDataSet.ValuePosition.OUTSIDE_SLICE);
 
         PieData pieData = new PieData(dataSet);
         pieData.setValueFormatter(new PercentFormatter());
         pieData.setValueTextSize(11f);
-        pieData.setValueTextColor(Color.DKGRAY);
+        pieData.setValueTextColor(pieDrawValuesColor);
+
         //是否绘制对应的数值比例
-        pieData.setDrawValues(false);
+        pieData.setDrawValues(isDrawCenterText);
+
 
         setData(pieData);
         highlightValues(null);
